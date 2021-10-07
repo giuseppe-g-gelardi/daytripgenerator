2f40d18c837ff8ce63820f8d364a2931a053b4c8
n-rdy <giuseppegelardi@icloud.com>
2021-10-07 07:30:10 -0500

    rearranged app structure, declared trip array as a more global variable so the user can reroll specific activities instead of the whole trip

--- app.js
+++ app.js
@@ -15,30 +15,7 @@ dataLogger(data)
 
 
 // ! function to get a random trip
-const getRandomTrip = (trip) => {
 
-  let tripArray = []
-
-  let randomDestination = Math.floor(Math.random() * trip.length) 
-
-  let rest = trip[randomDestination].restaurant
-  let randomRestaurant = Math.floor(Math.random() * rest.length)
-
-  let transportation = trip[randomDestination].transport
-  let randomTransport = Math.floor(Math.random() * transportation.length)
-
-  let ent = trip[randomDestination].entertainment
-  let randomEntertainment = Math.floor(Math.random() * ent.length)
-
-  tripArray.push(trip[randomDestination].destination)
-  tripArray.push(trip[randomDestination].restaurant[randomRestaurant])
-  tripArray.push(trip[randomDestination].transport[randomTransport])
-  tripArray.push(trip[randomDestination].entertainment[randomEntertainment])
-
-  return tripArray
-  
-
-}
 
 // getRandomTrip(data)
 // let tripLogger = getRandomTrip(data)
@@ -46,8 +23,8 @@ const getRandomTrip = (trip) => {
 // !
 
 
-
-const reRollTripActivities = () => {
+// ! function to reroll specific activities
+const reRollTripActivities = (data) => {
   // figure out how to reroll specific things here plz halp
 }
 
@@ -56,23 +33,54 @@ const reRollTripActivities = () => {
 
 
 
-const app = () => {
+const app = (trip) => {
+  let tripArray = []
+
+
+  const getRandomTrip = () => {
+
+  
+    let randomDestination = Math.floor(Math.random() * trip.length) 
+  
+    let rest = trip[randomDestination].restaurant
+    let randomRestaurant = Math.floor(Math.random() * rest.length)
+  
+    let transportation = trip[randomDestination].transport
+    let randomTransport = Math.floor(Math.random() * transportation.length)
+  
+    let ent = trip[randomDestination].entertainment
+    let randomEntertainment = Math.floor(Math.random() * ent.length)
+  
+    tripArray.push(trip[randomDestination].destination)
+    tripArray.push(trip[randomDestination].restaurant[randomRestaurant])
+    tripArray.push(trip[randomDestination].transport[randomTransport])
+    tripArray.push(trip[randomDestination].entertainment[randomEntertainment])
+  
+    return tripArray
+    
+  }
+
+
+
+
+
   let isComplete = false;
 
   while (!isComplete) {
-    console.log(getRandomTrip(data))
+    console.log(getRandomTrip())
     let userInput = prompt('Are you satisfied with your trip? Yes or No').toLowerCase()
 
     if (userInput === "yes") {
       isComplete = true;
       console.log("Enjoy your trip")
     } else {
-      getRandomTrip(data)
+      getRandomTrip()
+      console.log(tripArray)
     }
 
   }
 
 }
-app()
+// app()
 
 
