0e45614c0d3745e33f52e4ab8ba979c0f5ece0f0
n-rdy <giuseppegelardi@icloud.com>
2021-10-06 19:58:00 -0500

    started the app function that initiates the program that generates a trip.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
-// ! day trip generator!
+// * day trip generator!
 
-
-
+// ! this just logs data to the console
 const dataLogger = (trip) => {
   console.log(trip)
 }
 dataLogger(data)
+// ! plz no touchy
 
+
+
+
+
+
+
+// ! function to get a random trip
 const getRandomTrip = (trip) => {
 
-
+  let tripArray = []
 
   let randomDestination = Math.floor(Math.random() * trip.length) 
   let transportation = trip[randomDestination].transport
@@ -18,18 +25,32 @@ const getRandomTrip = (trip) => {
   let ent = trip[randomDestination].entertainment
   let randomEntertainment = Math.floor(Math.random() * ent.length)
 
+  tripArray.push(trip[randomDestination].destination)
+  tripArray.push(trip[randomDestination].restaurant)
+  tripArray.push(trip[randomDestination].transport[randomTransport])
+  tripArray.push(trip[randomDestination].entertainment[randomEntertainment])
 
-  console.log(trip[randomDestination].destination)
-  console.log(trip[randomDestination].restaurant)
-  console.log(trip[randomDestination].transport[randomTransport])
-  console.log(trip[randomDestination].entertainment[randomEntertainment])
-
-}
-getRandomTrip(data)
-
-const isSatisfied = () => {
-  // let satisfied = false;
-
+  return tripArray
 
 }
-isSatisfied()
+
+// getRandomTrip(data)
+// let tripLogger = getRandomTrip(data)
+// console.log(tripLogger)
+// !
+
+
+
+
+
+
+
+
+
+const app = () => {
+  let trip = getRandomTrip(data)
+  console.log(trip)
+}
+app()
+
+
